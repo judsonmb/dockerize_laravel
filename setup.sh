af058b0de5d318e4dde48d610fb4b6150861c3f3
Judson Bandeira <judsonmelobandeira@gmail.com>
2025-03-09 09:06:50 -0300

    Adding nodejs and npm install to run npm run build.

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Corrige as permissões do Laravel
+echo "Corrigindo permissões..."
+docker exec -it php bash -c "chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache"
+docker exec -it php bash -c "chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache"
+
+# Cria o banco de dados (no container do MySQL)
+echo "Criando banco de dados..."
+docker exec -it db bash -c "mysql -uroot -proot -e 'CREATE DATABASE IF NOT EXISTS laravel;'"
+
+# Gera a chave da aplicação Laravel
+echo "Gerando a chave da aplicação..."
+docker exec -it php bash -c "cd application && php artisan key:generate"
+
+# Executa as migrações do Laravel
+echo "Rodando migrações..."
+docker exec -it php bash -c "php artisan migrate && npm run build"
+
+echo "Configuração finalizada!"
